8717883ac7b66366b4668e9f1f5cb08952aadce2
sakaizawa <ksf.doingmorewithless@gmail.com>
2015-10-26 16:10:14 +0900

    first commit

---
+++ src/main/java/com/github/sakaizawa/ParseTree/BreadthFirstTreeIterator.java
@@ -0,0 +1,27 @@
+package com.github.sakaizawa.ParseTree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by sakaisawayuya on 2015/10/26.
+ */
+public class BreadthFirstTreeIterator
+        implements Iterator<ParseTree> {
+
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public ParseTree next() {
+        return null;
+    }
+
+    public void remove() {
+
+    }
+}
