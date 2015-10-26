6f21a5bcd3415a9d5d3e79a4a3fc9e927b30ae43
sakaizawa <ksf.doingmorewithless@gmail.com>
2015-10-26 16:09:38 +0900

    first commit

---
+++ src/main/java/com/github/sakaizawa/ParseTree/DepthFirstTreeIterator.java
@@ -0,0 +1,25 @@
+package com.github.sakaizawa.ParseTree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by sakaisawayuya on 2015/10/26.
+ */
+public class DepthFirstTreeIterator
+    implements Iterator<ParseTree> {
+
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public ParseTree next() {
+        return null;
+    }
+
+    public void remove() {
+
+    }
+}
